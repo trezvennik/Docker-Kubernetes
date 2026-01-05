763ac64a6050f275aba492b49b3a7df3962332ab
sergo <bulliom@ya.ru>
2026-01-05 08:57:45 +0300

    Update welcome message and creator in index.php

--- Docker_Image/Apache_php/index.php
+++ Docker_Image/Apache_php/index.php
@@ -1,7 +1,8 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
 
-echo "<h1>Welcome to the Simple Website based on PHP</h1><br>";
-echo "<h2>Created by <font color=orange>Matvey Guralskiy</font></h2><br>";
-echo "<p>Your IP address is: $ip_server</p><br>";
+echo "<h1>Hello from Kubernetes</h1><br>";
+echo "Server IP Address is: " . $ip_server . "<br><p>";
+echo "Made by <font color=blue>SeniorDevOpsSergio</font>";
+
 ?>
